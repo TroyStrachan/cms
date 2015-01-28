5574878e79b7bbc12bcfca84a8c243dd243a9afe
Troy Strachan <troy.strachan@gmail.com>
2015-01-28 10:31:51 -0800

    Updated HW.
    Client side working as well as server side

--- shared/flickr.js
+++ shared/flickr.js
@@ -1,19 +1,31 @@
 // shared/flickr.js
-// input should be flickr's photo details including server secret and farm
-// output is full jpg web address
-var createJpgPath = function(photos)
-{
+
+// Input[] should be Flickr's photo details including server, secret, farm
+// Output[] is the full JPG web address
+var createJpgPath = function (photos) {
 	var i,
 		photo,
-		photoSrc=[],
+		photoSrc = [],
 		len = photos.length;
 	for (i = 0; i < len; i++) {
 		photo = photos[i];
 		// https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 		photoSrc.push("https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg");
-		}
-	return photoSrc;	
+	}
+	return photoSrc;
 };
-if (typeof module !== "undefined"){
-	module.exports.createJpgPath = createJpgPath
+
+var myflickrOptions = {
+	"url": 'https://api.flickr.com/services/rest/',
+	"qs": {
+		"method": 'flickr.photos.search', // people.getPublicPhotos
+		"format": 'json',
+		"nojsoncallback": 1
+	},
+	"json": true
+};
+
+if (typeof module !== "undefined") {
+	module.exports.createJpgPath = createJpgPath;
+	module.exports.myflickrOptions = myflickrOptions;
 }
