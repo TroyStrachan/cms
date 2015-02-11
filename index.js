7ecd85f034a0293cf13420c818951dfcc267ac84
Troy Strachan <troy.strachan@gmail.com>
2015-02-11 11:06:10 -0800

    Twitter
    able to get data through twtterClient

--- index.js
+++ index.js
@@ -40,6 +40,38 @@ server.route({
 				reply(srcTag);
 			}
 		})
+	},
+	method: 'GET',
+    path:'/twitter',
+	handler: function (request, reply) {
+		var credentials = require('./credentials.js'),
+			httpRequest = require('request'),
+			flickrLib= require('./shared/flickr.js'),
+			flickr = {
+				"url": 'https://api.flickr.com/services/rest/',
+				"qs": {
+					"method": 'flickr.photos.search',
+					"api_key": credentials.flickr.api_key,
+					"tags": 'seabus',
+					"format": 'json',
+					"nojsoncallback": 1
+				},
+				"json": true
+			};
+		httpRequest(flickr, function (error, incomingMessage, response) {
+			if (!error && incomingMessage.statusCode === 200) {
+				// todo inclass: output html images
+				// reply(flickrLib.createJpgPath(response.photos.photo)); // Browser output
+				var photoSrc = flickrLib.createJpgPath(response.photos.photo),
+					srcTag="",
+					i;
+					for (i=0; i<photoSrc.length; i++)
+					{
+						srcTag += '<img src="'+photoSrc[i]+'">';
+					} 
+				reply(srcTag);
+			}
+		})
 	}
 });
 server.route({
